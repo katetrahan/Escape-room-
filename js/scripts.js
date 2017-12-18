7ac93664646dc8b7a85b20afdfd6a425f34c2290
Jun Li <june11084@gmail.com>
2017-12-18 15:41:28 -0800

    have objects, prototype functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 //business logic
+
+function Items(items){
+  this.items=items;
+}
+
+function Door(){
+  this.door="Door"
+}
+
+Items.prototype.checkItems=function(){
+  if(this.items.toString().match(/(?=.*key)/)){
+    alert("Proceed!")
+  } else {
+    alert("It won't budge, it is locked")
+  }
+}
+
+$(document).ready(function(){
+
+
+
+  Door.onClick = checkItems();
+
+});
