a24dd234184aca6ec1b44f9324ed50147c9e68ea
julia sheremet and kate trahan <julia+kate@>
2017-12-20 10:56:07 -0800

    add page 2 js, clickable objects and tally of inventory

--- js/scripts.js
+++ js/scripts.js
@@ -20,9 +20,9 @@ Items.prototype.checkItemsLevelOne=function(){
 }
 //function to get to level 3
 Items.prototype.checkItemsLevelTwo=function(){
-  if(this.items.toString().match(/(?=.*book)(?=.*glasses)/)){
-    document.getElementById('room2').src= "room2/saturn.png";
-    document.getElementById("saturn").onclick=function(){
+  if(this.items.toString().match(/(?=.*book)(?=.*specs)/)){
+    // document.getElementById('room2').src= "room2/saturn.png";
+    document.getElementById("r2_saturn").onclick=function(){
       location.href = "roomthree.html";
     }
   } else {
@@ -57,16 +57,29 @@ $(document).ready(function(){
 //   var itemArray = [];
   $("#r2_book").click(function(){
     var book = "book"
-    $("#r2_book").fadeOut();
+    $("#r2_book").css("opacity", "0");
     itemArray.push(book);
     console.log(itemArray);
     $("#inventory").append("<li>Book</li>");
   });
-  // var items = new Items(itemArray);
 
   $("#r2-saturn").click(function(){
     items.checkItemsLevelOne();
   });
+
+  $("#r2_specs").click(function(){
+    var specs = "specs"
+    $("#r2_specs").css("opacity", "0");
+    itemArray.push(specs);
+    console.log(itemArray);
+    $("#inventory").append("<li>Bifocals</li>");
+  });
+
+  $("#r2_saturn").click(function(){
+    items.checkItemsLevelTwo();
+
+  });
+
 
   $("#secondLevelButton").click(function(){
     $("#level2Img").fadeOut();
